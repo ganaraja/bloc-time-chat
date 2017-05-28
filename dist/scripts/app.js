75c577b3ac6c74c9a664ef9e7ef5d034dfa3f5b3
Ganaraja NG <ganaraja@gmail.com>
2017-05-28 08:12:16 +0530

    Checkpoint 5

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -13,7 +13,31 @@
           })
   }
 
+  function BlocChatCookies($cookies, $uibModal) {
+      var currentUser = $cookies.get('blocChatCurrentUser');
+      console.log(currentUser);
+
+      if (!currentUser || currentUser === '') {
+
+          var modalInstance = $uibModal.open({
+              templateUrl: '/templates/usernameModal.html',
+              controller: 'usernameModalInstanceCtrl',
+              controllerAs: 'modalUN',
+              backdrop: 'static',
+              keyboard: false
+          });
+
+          modalInstance.result.then(function(username) {
+              this.username = username;
+              $cookies.put('blocChatCurrentUser', this.username);
+              var currentUser = $cookies.get('blocChatCurrentUser');
+              console.log(currentUser);
+          });
+      }
+  }
+
     angular
-        .module('blocChat', ['ui.router',"firebase","ui.bootstrap"])
-        .config(config);
+        .module('blocChat', ['ngCookies','ui.router',"firebase","ui.bootstrap"])
+        .config(config)
+        .run(['$cookies', '$uibModal', BlocChatCookies]);
 })();
---
+++ dist/scripts/controllers/usernameModalInstanceCtrl.js
@@ -0,0 +1,16 @@
+(function() {
+    function usernameModalInstanceCtrl($uibModalInstance, $scope) {
+        
+        $scope.ok = function(isValid) {
+            
+            if (isValid) {
+                $uibModalInstance.close($scope.username);
+            }
+            
+        };
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('usernameModalInstanceCtrl', ['$uibModalInstance', '$scope', usernameModalInstanceCtrl]);
+})();
